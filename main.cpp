a2c5a8b3d5b7160c628de0bdcc0a7c118d905dc6
leelohoon <49421109+LeeLoHoon@users.noreply.github.com>
2019-05-21 02:43:50 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main() {
   ifstream fp,ep;
   int size,index,key;
   string fileName1,fileName2,value,line;
-  string source, target,temp;
+  string source, target,temp,fixed;
   
 
   try{
@@ -38,8 +38,10 @@ int main() {
   while(!ep.eof()){
     getline(ep,line);
     fixed=myBST->search(line);
+    cout<<fixed<<endl;
   //BST는 끝, 구현 할 것:  line을 파라미터로 search 실행하면 search 메소드 안에서 map tree의 key들을 빠짐없이 compare하여 같은 것이 있으면 value값으로 replace  그리고 string return
   }
   
 }
+  
   
