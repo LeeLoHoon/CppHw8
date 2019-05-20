470d69a401e321e09919f55217312f80d1ef9a82
leelohoon <49421109+LeeLoHoon@users.noreply.github.com>
2019-05-21 00:10:36 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,29 +8,38 @@ using namespace std;
 int main() {
   BST *myBST;
   myBST= new BST();
-  ifstream fp;
-  string fileName,value;
-  char command;
-  int key;
+  ifstream fp,ep;
+  int size,index,key;
+  string fileName1,fileName2,value,line;
+  string source, target,temp;
+  
+
   try{
-    cin >> fileName;
-    fp.open(fileName);
+    cin>>fileName1;
+    ep.open(fileName1);
+    cin >> fileName2;
+    fp.open(fileName2);
   }catch(const ifstream::failure& e){
    	cerr << "Exception occurred while opening the file." << endl;}
   while(!fp.eof()){
-    fp>>command;
-    if(command=='a'){
-      fp>>key;
-      fp>>value;
-      myBST->Add(key,value);
+    getline(fp,line);
+    size=line.size();
+    index=line.find(":");
+    
+    source=line.substr(0,index);
+    target=line.substr(index+1);
+    if(target[0]==' '){
+      target=target.substr(1);
     }
-    else if(command=='d'){
-      fp>>key;
-      myBST->Delete(key);
+
+    myBST->Add(source,target);
     }
-    else if(command=='p'){
-      myBST->Print();
-    }
-    else if(command=='q') break;
+  fp.close();
+  while(!ep.eof()){
+    getline(ep,line);
+    fixed=myBST->search(line);
+  //BST는 끝, 구현 할 것:  line을 파라미터로 search 실행하면 search 메소드 안에서 map tree의 key들을 빠짐없이 compare하여 같은 것이 있으면 value값으로 replace  그리고 string return
   }
+  
 }
+  
