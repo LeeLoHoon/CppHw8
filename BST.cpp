709af06ac7d2ced618b3bdd3d958099e16243cae
leelohoon <49421109+LeeLoHoon@users.noreply.github.com>
2019-05-23 00:10:53 +0900

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -1,122 +1,50 @@
 #include "BST.h" 
 #include <iostream>
+#include <fstream>
 #include <string>
 
 using namespace std;
 
-string BST::search(tree_node *tree,string line){
-  int a,b;
-  str c,newLine,start,medium,end;
-  if(tree == NULL)
-    return NULL;
-  search(tree->left_child,line);
-  a=line.find(tree->str1);
-  b=tree->str1.lenth();
-  strat=line.substr(0,a);
-  medium=tree->str2;
-  end=line.substr(a+b-1);
-  newLine=start+medium+end;
-  if()
-  //cout<<"("<<tree->str1<<","<<tree->str2<<")"<<endl;
-  search(tree->right_child,line);
+int main() {
+  BST *myBST;
+  myBST= new BST();
+  ifstream fp,ep;
+  int size,size2,index;
+  string fileName1,fileName2,value,line;
+  string source, target,temp,fixed;
   
-  return newLine;
 
-/*  if(tree==NULL){
-    return NULL;  
+  try{
+    cin>>fileName1;
+    ep.open(fileName1);
+    cin >> fileName2;
+    fp.open(fileName2);
+  }catch(const ifstream::failure& e){
+   	cerr << "Exception occurred while opening the file." << endl;}
+  while(!fp.eof()){
+    getline(fp,line);
+    size=line.size();
+    index=line.find(":");
+    
+    source=line.substr(0,index);
+    target=line.substr(index+1,size);
+    if(target[0]==' '){
+      target=target.substr(1);
+    }
+    size2=target.size();
+    if(target[size2-1]==' '){ 
+      target=target.substr(0,size2-1);
+    }
+    //cout<<target<<endl;
+    myBST->Add(source,target);
+    }
+  fp.close();
+  while(!ep.eof()){
+    getline(ep,line);
+    fixed=myBST->search(line);
+    cout<<fixed<<endl;
+  //BST는 끝, 구현 할 것:  line을 파라미터로 search 실행하면 search 메소드 안에서 map tree의 key들을 빠짐없이 compare하여 같은 것이 있으면 value값으로 replace  그리고 string return
   }
-  while(tree!=NULL){
-    if(key.compare(tree->str1)==0) return tree->str2;
-    else if (key.compare(tree->str1)<0)
-      if(tree->left_child == NULL) return NULL;
-      else tree=tree->left_child;
-    else                       
-      if (tree->right_child == NULL) return NULL; 
-      else tree = tree->right_child;
-  }
-  return NULL;
-}*/
-string BST::search(string line){
-  return search(root, key);
+  ep.close();
 }
-
-
-tree_node* BST::Add(tree_node* tree, string key, string value){
-  if(tree==NULL){
-    tree = new tree_node;
-    tree->str1 = key;
-    tree->str2 = value;
-    tree->left_child=NULL;
-    tree->right_child=NULL;
-  }  
-  else if(key.compare(tree->str1)==0){
-    cout<<key<<" is already in the tree."<<endl;
-  } 
-  else{
-    if(key.compare(tree->str1)>0){
-      tree->right_child = Add(tree->right_child,key,value);}
-    else if(key.compare(tree->str1)<0){
-      tree->left_child=Add(tree->left_child,key,value);
-    }
-}
-  return tree;
-}
-tree_node* BST::Delete(tree_node* tree,string key){
-  tree_node* temp;
-  if(key.compare(tree->str1)==0){
-    if((tree->left_child==NULL)&&(tree->right_child==NULL)){
-      tree=NULL;
-      return tree;}
-    else if(tree->left_child==NULL){
-      temp=tree;
-      tree=tree->right_child;
-      delete temp;
-    }
-    else if(tree->right_child==NULL){
-      temp=tree;
-      tree=tree->left_child;
-      delete temp;
-    }
-    else{
-      string a;
-      string b;
-      temp=findMin(tree->right_child);
-      a=temp->str1;
-      b=temp->str2;
-      Delete(a);
-      tree->str1=a;
-      tree->str2=b;
-    }
-  }
-  else if(key.compare(tree->str1)>0){
-    if(tree->right_child==NULL){
-      cout<<key<<" is not in the tree."<<endl;
-      return tree;}
-    tree->right_child=Delete(tree->right_child,key);
-    }
-  else if(key.compare(tree->str1)<0){
-    if(tree->left_child==NULL){
-      cout<<key<<" is not in the tree."<<endl;
-      return tree;}
-    tree->left_child=Delete(tree->left_child,key);
-    }
-  return tree;
-}
-tree_node* BST::findMin(tree_node* tree){
-  tree_node* temp=NULL;
-  if(tree->left_child==NULL){
-    temp=tree;
-    return temp;}
-  else {
-    tree=tree->left_child;
-    findMin(tree);
-    return tree;
-  }
-}
-void BST::Print(tree_node* tree){
-  if(tree == NULL)
-    return;
-  Print(tree->left_child);
-  cout<<"("<<tree->str1<<","<<tree->str2<<")"<<endl;
-  Print(tree->right_child);
-}
+  
